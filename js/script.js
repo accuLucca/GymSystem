836e844df51e30ad348ff8c1dab979786eab2e76
RicardoFuzetti <rickfuzetti12@gmail.com>
2021-03-19 20:45:40 -0300

    mensagem erro
    Implementado mensagem de erro caso os campos estiverem vazios e implementado função para limpar os campos

--- js/script.js
+++ js/script.js
@@ -4,5 +4,38 @@ const Modal = {
     },
     close() {
         document.querySelector('.modal-overlay').classList.remove('ativo')
+        document.querySelector('.modal-erro').classList.remove('ativo')
+        Login.limparCampo()
+    }
+}
+
+const Login = {
+    usuario: document.getElementById('inputUsuario'),
+    senha: document.getElementById('inputSenha'),
+
+    getValues() {
+        return {
+            usuario: Login.usuario.value,
+            senha: Login.senha.value
+        }
+    },
+
+    validarCampo() {
+        const { usuario, senha } = Login.getValues()
+        if (usuario.trim() === "" || senha.trim() === "") {
+            document.querySelector('.modal-erro').classList.add('ativo')
+        }else{
+            console.log("CAMPOS PREENCHIDOS")
+        }
+    },
+
+    limparCampo() {
+        const { usuario, senha } = Login.getValues()
+        if (usuario.trim() !== "" || senha.trim() !== "") {
+            Login.usuario.value = ""
+            Login.senha.value = ""
+        }else{
+            console.log("CAMPOS VAZIOS")
+        }
     }
 }
