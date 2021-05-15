cc63ac112e2010eed32875cd97c01c3e14f537ee
RicardoFuzetti <rickfuzetti12@gmail.com>
2021-05-14 22:58:51 -0300

    exclusao funcionando, porem com BUG

--- php/excluirAluno_exe.php
+++ php/excluirAluno_exe.php
@@ -2,10 +2,17 @@
 
     require 'conectaBD.php';
     
-    $matricula = $_POST[''];
+    $matricula = $_POST['matricula'];
     
     // Faz Select na Base de Dados
 	$sql = "DELETE FROM Aluno WHERE matricula = $matricula";
 
+    if ($result = mysqli_query($conn, $sql)) {
+        echo "Um registro excluído!";
+    } else {
+    echo "Erro executando DELETE: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);  //Encerra conexao com o BD
 
 ?>
--- php/listarAlunos.php
+++ php/listarAlunos.php
@@ -41,7 +41,7 @@
             echo "<table>";
 
             if ($result = mysqli_query($conn, $sql)) {
-                
+
                 echo "<tr>";
                 echo "<th width='3%'>";
                 echo "<h3> Matrícula </h3>";
@@ -69,12 +69,11 @@
                     echo "<tr class= 'border_bottom'>";
                     echo "<th> <span>" . $cod . "</span> </th>";
                     echo "<th> <span>" . $row["nome"] . "</span> </th>";
-                    echo "<th> <span>" . $row ["telefone"] . "</span> </th>";
+                    echo "<th> <span>" . $row["telefone"] . "</span> </th>";
                     echo "<th> <span>" . $nova_data . "</span> </th>";
                     echo "<th> <button class='botoes'> <img src='../img/edit.svg'> </button> </th>";
                     echo "<th> <button class='botoes' onclick='ModalExcluirAluno.open()'> <img src='../img/trash.svg' > </th>";
                     echo "</tr>";
-                    
                 }
             }
             echo "</table>";
@@ -83,7 +82,7 @@
             echo "</div>";
             echo "</div>";
             echo "</div>";
- 
+
 
             mysqli_close($conn);
             ?>
@@ -131,16 +130,43 @@
     <div class="modal-overlay-excluir">
 
         <div class="modal">
+            <?php
 
-            <h2>Tem certeza que deseja excluir o aluno?</h2>
-            <form action="../php/excluirAluno_exe.php" method="post">
+            require 'conectaBD.php';
 
-                <div class="divButtonModal">
-                    <button class="bCancelar" type="button" onclick="ModalExcluirAluno.close()">Cancelar</button>
-                    <button class="bCadastar" type="submit" onclick="">Excluir</button>
-                </div>
+            // Cria conexão
+            $conn = mysqli_connect($servername, $username, $password, $database);
 
-            </form>
+            // Verifica conexão
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            // Faz Select na Base de Dados
+            $sql = "SELECT matricula FROM Aluno";
+            if ($result = mysqli_query($conn, $sql)) {
+                if (mysqli_num_rows($result) > 0) {
+                    // Apresenta cada linha da tabela
+                    while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+                        <h2>Tem certeza que deseja excluir o aluno <?php echo $row['matricula'] ?> ?</h2>
+                        <form action="../php/excluirAluno_exe.php" method="post">
+
+                            <div class="divButtonModal">
+                                <input type="hidden" id="matricula" name="matricula" value="<?php echo $row['matricula']; ?>">
+                                <button class="bCancelar" type="button" onclick="ModalExcluirAluno.close()">Cancelar</button>
+                                <button class="bCadastar" type="submit">Excluir</button>
+                            </div>
+
+                        </form>
+            <?php
+                    }
+                }
+            } else {
+                echo "Erro executando DELETE: " . mysqli_error($conn);
+            }
+            mysqli_close($conn);  //Encerra conexao com o BD
+            ?>
         </div>
     </div>
 
