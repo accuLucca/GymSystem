a6d37d1db324a7a13ab860080e2e771d61a8c3d0
RicardoFuzetti <rickfuzetti12@gmail.com>
2021-06-07 17:28:33 -0300

    graficooooooooo

--- js/chart.js
+++ js/chart.js
@@ -1,15 +1,38 @@
-var ctx = document.getElementById('myChart').getContext('2d');
+$('document').ready(function () {
 
-var chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: '#150485',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [5, 10, 5, 2, 20, 50, 10],
-        }],
-    },
-    options: {},
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "../php/grafico.php",
+        success: function (data) {
+
+            var horaMarcada = [];
+
+            for (var i in data) {
+                horaMarcada.push(data[i].horaMarcada);
+                console.info(data[i].horaMarcada);
+            }
+            grafico(horaMarcada);
+        }
+    });
+
 })
+
+function grafico(hora) {
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'],
+            datasets: [{
+                label: 'Pessoas',
+                backgroundColor: '#150485',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            }],
+        },
+        options: {},
+    })
+}
---
+++ php/grafico.php
@@ -0,0 +1,17 @@
+        <?php
+
+        $pdo = new PDO('mysql:host=localhost;dbname=academia;port=3307;chartset=utf8', 'Ricardo', '1234@puc');
+
+        $sql = "SELECT count(agenda.hora) FROM Agenda WHERE agenda.hora = agenda.hora GROUP BY agenda.hora";
+
+        $statement = $pdo->prepare($sql);
+
+        $statement->execute();
+
+        while ($results = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $result[] = $results;
+        }
+
+        echo json_encode($result);
+
+        ?>
